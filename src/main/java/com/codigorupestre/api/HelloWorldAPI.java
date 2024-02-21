8fc46719a75a3821d144971ff76239b38f3d544d
EduardoCM <ecm3198@gmail.com>
2024-02-20 22:41:26 -0600

    se actualiza

--- src/main/java/com/codigorupestre/api/HelloWorldAPI.java
+++ src/main/java/com/codigorupestre/api/HelloWorldAPI.java
@@ -16,7 +16,7 @@ public class HelloWorldAPI {
 	public ResponseEntity<Object> creteUser() {
         log.info("Hello World");
 		
-		return ResponseEntity.ok("Hello World I am expert Java Full Stack Developer");
+		return ResponseEntity.ok("Hello World I am expert Dos Dos Java Full Stack Developer");
 	}
 
 }
