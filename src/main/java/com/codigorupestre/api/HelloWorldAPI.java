0d2e97c8b89ab140b79681669c18b5863833a861
EduardoCM <ecm3198@gmail.com>
2024-02-24 13:33:16 -0600

    update proyect

--- src/main/java/com/codigorupestre/api/HelloWorldAPI.java
+++ src/main/java/com/codigorupestre/api/HelloWorldAPI.java
@@ -6,23 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
 public class HelloWorldAPI {
 	
 	@GetMapping
 	public ResponseEntity<Object> creteUser() {
-        log.info("Hello World");
+        System.out.println("Hello World I am expert Full Stack");
 		
 		return ResponseEntity.ok("Hello World I am expert Dos Dos Java Full Stack Developer");
 	}
 	
 	@DeleteMapping
 	public ResponseEntity<Object> eliminarUser() {
-        log.info("Delete Hello World");
+        System.out.println("By Iam not a expert full Stack");
 		
 		return ResponseEntity.ok("Iam not a expert  Java Full Stack Developer");
 	}
